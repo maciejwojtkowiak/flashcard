fbdcc063eab8636e80947c4a368a3da09e36d6d4
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-17 21:04:00 +0200

    initial

---
+++ components/Main/Main.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div className="h-screen bg-red-500 grid place-items-center">
+      <div className="h-[40rem] w-[30rem] bg-white"></div>
+    </div>
+  );
+};
+
+export default Main;
