9234df313b4b320874349561e0cb2079642b033c
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-17 23:15:45 +0200

    added adding items logic

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { listAction } from "../../src/store/list-slice";
+
 const Main = () => {
+  const dispatch = useDispatch();
+  const [itemName, setItemName] = useState<string>("");
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItemName(e.target.value);
+  };
+  const onSubmitHandler = () => {
+    dispatch(listAction.addNameToList(itemName));
+  };
   return (
     <div className="h-screen bg-red-500 grid place-items-center">
-      <div>hej</div>
+      <div className="h-[40rem] w-[30rem] bg-white flex">
+        <form className="h-[10%] w-full self-end">
+          <input onChange={onChangeHandler} className="w-full" />
+          <button type="submit" onSubmit={onSubmitHandler}>
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
