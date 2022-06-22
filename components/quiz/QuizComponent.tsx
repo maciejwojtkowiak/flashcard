9506b8b02691b63ad649ee301e54c2cc6fe1b7d9
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-22 19:45:15 +0200

    added progress bar

--- components/quiz/QuizComponent.tsx
+++ components/quiz/QuizComponent.tsx
@@ -1,5 +1,5 @@
 import { FlashCard } from "../../shared/types";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 interface QuizProps {
@@ -10,7 +10,9 @@ const QuizComponent: React.FC<QuizProps> = (props) => {
   const items = props.flashcard?.items;
   const MIN_INDEX = 0;
   const MAX_INDEX = items.length - 1;
+  const widthOfBlock = (100 / items.length).toString();
   const [actualIndex, setActualIndex] = useState<number>(0);
+  const arrayOfProgressBlocks: React.ReactElement[] = [];
   const [isAtMinIndex, setIsAtMinIndex] = useState<boolean>(false);
   const [isAtMaxIndex, setIsAtMaxIndex] = useState<boolean>(false);
 
@@ -28,10 +30,17 @@ const QuizComponent: React.FC<QuizProps> = (props) => {
     if (actualIndex === MAX_INDEX) setIsAtMaxIndex(true);
     else setIsAtMaxIndex(false);
   }, [actualIndex]);
+
+  for (let i = 0; i <= actualIndex; i++) {
+    arrayOfProgressBlocks.push(
+      <div className={`w-[${widthOfBlock}%]  border-2 border-blue-500`} />
+    );
+  }
+  console.log(widthOfBlock);
   return (
     <div className="h-screen w-full grid place-items-center">
-      <div className="h-[20rem] w-1/4 drop-shadow-2xl shadow-2xl text-blue-500 border-2">
-        <h1>{items[actualIndex]}</h1>
+      <div className="h-[20rem] w-1/4 grid place-items-center drop-shadow-2xl shadow-2xl text-blue-500 border-2">
+        <h1 className="text-6xl">{items[actualIndex]}</h1>
       </div>
 
       <div>
@@ -46,6 +55,7 @@ const QuizComponent: React.FC<QuizProps> = (props) => {
           </button>
         )}
       </div>
+      <div className=" w-full place-self-end flex">{arrayOfProgressBlocks}</div>
     </div>
   );
 };
